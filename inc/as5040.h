6fb08c93de77064d8cfe7eaaf60f15de4120834a
DESKTOP-RPNT1OB\Miroslav Bozic <miroslav.bozic@htecgroup.com>
2018-07-27 15:04:46 +0200

    Added as5040.h and as5040.c

---
+++ inc/as5040.h
@@ -0,0 +1,13 @@
+/*
+ * as5040.h
+ *
+ *  Created on: Jul 27, 2018
+ *      Author: Miroslav Bozic
+ */
+
+#ifndef AS5040_H_
+#define AS5040_H_
+
+
+
+#endif /* AS5040_H_ */
---
+++ src/as5040.c
@@ -0,0 +1,8 @@
+/*
+ * as5040.c
+ *
+ *  Created on: Jul 27, 2018
+ *      Author: Miroslav Bozic
+ */
+#include "as5040.h"
+
